29745988bbdef379215f4183908dcc820bd367a9
Melisssa_M <anastasiia.shevchenko.dev@gmail.com>
2021-12-10 17:47:11 +0200

    initializing needed tests

---
+++ conftest.py
@@ -0,0 +1,20 @@
+import pytest
+from selenium import webdriver
+import time
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default="en-gb",
+                     help="Choose language")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    language = request.config.getoption("language")
+    browser = webdriver.Chrome()
+    browser.implicitly_wait(12)
+    link = f"http://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207/"
+    browser.get(link)
+    yield browser
+    time.sleep(5)
+    browser.quit()
