adc56705433f57317fead3553818faed26c9e8ab
Melisssa_M <anastasiia.shevchenko.dev@gmail.com>
2021-12-13 14:35:44 +0200

    Added 3-step check of login page

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,9 @@
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
+import pytest
 
 
+@pytest.mark.skip
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
@@ -8,9 +11,40 @@ def test_guest_can_go_to_login_page(browser):
     page.go_to_login_page()
 
 
+@pytest.mark.skip
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
 
+
+@pytest.mark.skip
+def test_guest_should_see_login_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_form()
+
+
+@pytest.mark.skip
+def test_guest_should_see_registration_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_register_form()
+
+
+@pytest.mark.skip
+def test_guest_should_see_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_url()
+
+
+def test_guest_should_see_login_page_full_check(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_page()
