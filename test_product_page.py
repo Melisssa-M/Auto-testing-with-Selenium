f90ba52edd941ad9010ae031798629777b996754
Melisssa_M <anastasiia.shevchenko.dev@gmail.com>
2021-12-15 12:07:59 +0200

    Adding product to basket function was corrected(2 functions united) and parametrized offer check added

--- test_product_page.py
+++ test_product_page.py
@@ -3,20 +3,15 @@ from pages.product_page import ProductPage
 import pytest
 
 
-@pytest.mark.skip
-def test_guest_can_add_product_to_basket(browser):
-    link = f'http://selenium1py.pythonanywhere.com/{browser.language}/catalogue/coders-at-work_207/?promo=newYear2019'
+@pytest.mark.parametrize('promo_number', ["offer0", "offer1", "offer2", "offer3", "offer4", "offer5", "offer6", pytest.param("offer7", marks=pytest.mark.xfail), "offer8", "offer9"])
+def test_guest_can_add_product_to_basket(browser, promo_number):
+    link = f'http://selenium1py.pythonanywhere.com/{browser.language}/catalogue/coders-at-work_207/?promo={promo_number}'
     product_page = ProductPage(browser, link)
     product_page.open()
     browser.find_element(*ProductPageLocators.ADD_TO_BASKET).click()
     product_page.solve_quiz_and_get_code()
-
-
-def test_price_and_name_are_same_in_basket(browser):
-    test_guest_can_add_product_to_basket(browser)
     price_of_item = browser.find_element(*ProductPageLocators.PRICE_OF_ITEM).text
     price_pop_up = browser.find_element(*ProductPageLocators.PRICE_POP_UP).text
     name_of_item = browser.find_element(*ProductPageLocators.NAME_OF_ITEM).text
     name_pop_up = browser.find_element(*ProductPageLocators.NAME_POP_UP).text
-    assert price_of_item == price_pop_up and name_of_item == name_pop_up, f"{price_of_item} != {price_pop_up} or {name_of_item} != {name_pop_up} "
-
+    assert price_of_item == price_pop_up and name_of_item == name_pop_up, f"{price_of_item} != {price_pop_up} or {name_of_item} != {name_pop_up}, {promo_number}"
